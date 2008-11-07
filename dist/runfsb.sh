c141082fe29fceb9aec21208999eac9f5607cab8
mechie <mechie@d743d77d-d435-0410-a50a-c2528d3477d3>
2008-11-07 08:48:40 +0000

    Cleanout of 0.20
    git-svn-id: https://catacombae.svn.sourceforge.net/svnroot/catacombae@28 d743d77d-d435-0410-a50a-c2528d3477d3

--- dist/runfsb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-LIB=lib
-
-UNAME=`uname`
-if [ "${UNAME}" = "Darwin" ]; then
-    DOCK_NAME="-Xdock:name=HFSExplorer"
-fi
-java ${DOCK_NAME:+"${DOCK_NAME}"} -jar "$LIB/hfsx.jar" "$@"
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-LIB=dist/lib
-java -cp $LIB/hfsx.jar:$LIB/jparted.jar:$LIB/hfsx_dmglib.jar:$LIB/swing-layout-1.0.1.jar:$LIB/apache-ant-1.7.0-bzip2.jar:$LIB/iharder-base64.jar $1 $2 $3 $4 $5 $6 $7 $8 $9
